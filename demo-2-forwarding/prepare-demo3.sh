4a05db0d58836591b1a1cd006d2bc66e5ff385bf
Fabian Schulz <fabian.schulz1@swisscom.com>
2024-08-06 18:21:54 +0200

    added demo3 and improved demo 1 and 2

---
+++ demo-2-forwarding/prepare-demo3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Get the directory of the current script
+SCRIPT_DIR=$(dirname "$0")
+
+# Destroy the single kind cluster
+"$SCRIPT_DIR/../destroy-kind.sh" 1
+# Create 3 kind clusters
+"$SCRIPT_DIR/../create-kind-clusters.sh" 3
+# Deploy the multicluster dns in all 3 clusters
+"$SCRIPT_DIR/../setup-kind.sh" 0
+"$SCRIPT_DIR/../setup-kind.sh" 1
+"$SCRIPT_DIR/../setup-kind.sh" 2
